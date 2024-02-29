57f27b20e0e498026c5158f1fbe49437bfcd80d9
Shifra Eisen <shifraeisen@gmail.com>
2024-02-28 21:16:27 -0500

    Add project files.

---
+++ ExcerciseClassFeb28/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExcerciseClassFeb28.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
